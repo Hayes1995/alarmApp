ef103e383cb1009501e78536fda43209d5b630cd
Hayes <mcnelishayes@gmail.com>
2024-09-09 19:43:42 -0400

    newalarm

---
+++ lib/widgets/new_alarm.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewAlarm extends StatefulWidget {
+  NewAlarm({super.key});
+
+  @override
+  State<NewAlarm> createState() {
+    return _NewAlarmState();
+  }
+}
+
+class _NewAlarmState extends State<NewAlarm> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
